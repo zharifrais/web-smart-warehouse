4d3a2cf316d779581e41f8b13f8393e53b8952a6
zharifrais22 <zharifrais10@gmail.com>
2026-01-25 14:30:34 +0700

    include vendor for shared hosting deployment

--- config/mqtt.php
+++ config/mqtt.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'host'        => env('MQTT_HOST', '41bde8e7e4764a33ad0cf261439dafc9.s1.eu.hivemq.cloud'),
+    'host'        => env('MQTT_HOST', '7b3d2ad198b04c44bd1993be8c1c91c6.s1.eu.hivemq.cloud'),
     'port'        => env('MQTT_PORT', 8883),
     'username'    => env('MQTT_USERNAME', 'esp32user'),
-    'password'    => env('MQTT_PASSWORD', 'Esp32pass123'),
+    'password'    => env('MQTT_PASSWORD', 'Esp32username'),
     'client_id'   => env('MQTT_CLIENT_ID', 'laravel-subscriber-01'),
     'cafile'      => env('MQTT_SSL_CAFILE', null),
 ];
